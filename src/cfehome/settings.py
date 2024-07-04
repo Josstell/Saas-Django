ed7cf41cadbbac76bf44d2281dce7aae5fdb6548
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-07-04 17:55:33 -0600

    NEON DB

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -100,6 +100,18 @@ DATABASES = {
     }
 }
 
+CON_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CON_MAX_AGE,
+            conn_health_checks=True
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
