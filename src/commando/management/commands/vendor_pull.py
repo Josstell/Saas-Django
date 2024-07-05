e7de3aab0a5c783ed976d10330fe7e76f6c9bed9
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-07-05 16:47:49 -0600

    arg during build 3

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -1,4 +1,6 @@
 import helpers
+
+from typing import Any
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -6,8 +8,8 @@ STATICFILES_VENDOR_DIR = getattr(settings, "STATICFILES_VENDOR_DIR")
 
 VENDOR_STATICFILES = {
     # "saas-theme.min.css": "https://raw.githubusercontent.com/codingforentrepreneurs/SaaS-Foundations/main/src/staticfiles/theme/saas-theme.min.css",
-    "flowbite.min.css": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
-    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
+    "flowbite.min.css": "https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css",
+    "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js",
     # "flowbite.min.js.map": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js.map"
 }
 
